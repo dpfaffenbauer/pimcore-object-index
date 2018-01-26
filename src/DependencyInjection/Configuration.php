bcce9fb9b9328730659452833d36543510cbc80d
dpfaffenbauer <dominik@lineofcode.at>
2018-01-26 15:37:03 +0100

    remove unused uses

--- src/DependencyInjection/Configuration.php
+++ src/DependencyInjection/Configuration.php
@@ -2,8 +2,6 @@
 
 namespace ObjectIndexBundle\DependencyInjection;
 
-use CoreShop\Bundle\CoreBundle\Checkout\CheckoutManager;
-use CoreShop\Bundle\ResourceBundle\CoreShopResourceBundle;
 use Symfony\Component\Config\Definition\Builder\ArrayNodeDefinition;
 use Symfony\Component\Config\Definition\Builder\TreeBuilder;
 use Symfony\Component\Config\Definition\ConfigurationInterface;
